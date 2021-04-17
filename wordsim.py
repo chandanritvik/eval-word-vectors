a48977a126878ee0d2fcab5066a4a995e9677f08
chandanritvik <chandan.gurramluck@gmail.com>
2021-04-17 13:55:13 +0530

    Update wordsim.py

--- wordsim.py
+++ wordsim.py
@@ -1,17 +1,28 @@
 import sys
 import os
 
-from read_write import read_word_vectors
 from ranking import *
+
+def read_vectors(filename):    
+  word_vecs = {}
+  if filename.endswith('.gz'): file_object = gzip.open(filename, 'r')
+  else: file_object = open(filename, 'r')
+
+  for line_num, line in enumerate(file_object):
+    line = line.strip().lower()
+    word = line.split()[0]
+    word_vecs[word] = numpy.zeros(len(line.split())-1, dtype=float)
+    for index, vec_val in enumerate(line.split()[1:]):
+      word_vecs[word][index] = float(vec_val)      
+    ''' normalize weight vector '''
+    word_vecs[word] /= math.sqrt((word_vecs[word]**2).sum() + 1e-6)        
+  return word_vecs
 
 if __name__=='__main__':  
   word_vec_file = sys.argv[1]
   word_sim_file = sys.argv[2]
   
-  word_vecs = read_word_vectors(word_vec_file)
-  print '================================================================================='
-  print "%15s" % "Num Pairs", "%15s" % "Not found", "%15s" % "Rho"
-  print '================================================================================='
+  word_vecs = read_vectors(word_vec_file)
 
   manual_dict, auto_dict = ({}, {})
   not_found, total_size = (0, 0)
@@ -24,5 +35,4 @@ if __name__=='__main__':
     else:
       not_found += 1
     total_size += 1    
-  print "%15s" % str(total_size), "%15s" % str(not_found),
-  print "%15.4f" % spearmans_rho(assign_ranks(manual_dict), assign_ranks(auto_dict))
+  print (str(total_size), str(not_found),( spearmans_rho(assign_ranks(manual_dict), assign_ranks(auto_dict)))
